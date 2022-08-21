20e1e20a58171be311c5e14c2de374d7acc7cd45
unknown <dheerajdalbanjan0403@gmail.com>
2022-08-21 22:27:42 +0530

    updated for mobile

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ var messageElement ;
 const append = (message , position)=>{
     messageElement = document.createElement('div') ; 
     messageElement.innerText = message  ;
-    messageElement.classList.add('flex' , 'font-thin'  ,  'bg-slate-500'  , 'h-14' , 'w-fit' ,'md:rounded-full' ,'rounded-lg' , 'rounded-br-none'  , 'mx-7' , 'py-4' , 'px-8' , 'text-white' , 'max-w-[18rem]' , 'md:max-w-[40rem]' );
+    messageElement.classList.add('flex' , 'font-thin'  ,  'bg-slate-500'  , 'h-14' , 'w-fit' ,'md:rounded-full' ,'rounded-lg'   , 'mx-7'  , 'text-white' , 'max-w-[18rem]' , 'md:max-w-[40rem]' , 'md:py-4', 'md:px-8' ,'p-3');
     const array = position.split(" ") ;
     array.forEach(arrayf=> {
         messageElement.classList.add(arrayf) ;
@@ -20,13 +20,12 @@ const name = prompt("Enter your name" ) ;
 socket.emit(`new-user-joined` , name) ;
 
 socket.on(`user-joined` , name=>{
-    append(`${name} joined the chat` , 'self-end' + ' ' + 'bg-blue-300') ; 
+    append(`${name} joined the chat` , 'self-end' + ' ' + 'bg-blue-300' + ' ' +  'md:rounded-br-none') ; 
     
 })
 
 socket.on(`receive` , data =>{
-    append(`${data.name} : ${data.message} ` , 'self-start' +  ' ' + 'rounded-bl-none' + ' ' + 'md:rounded-bl-none' + 'rounded-br-xl') ; 
-    messageElement.classList.add('rounded-br-full' , 'rounded-bl-none') ; 
+    append(`${data.name} : ${data.message} ` , 'self-start' + ' ' + 'md:rounded-bl-none' + ' ' +  'rounded-br-xl') ; 
 })
 
 const input = document.querySelector("input") ;
@@ -35,12 +34,12 @@ document.querySelector("form").addEventListener('submit' , (e)=>{
     e.preventDefault() ;
     const message = input.value ; 
     input.value = "" ; 
-    append(`You : ${message}`, 'self-end')  ;
+    append(`You : ${message}`, 'self-end' + ' ' + 'md:rounded-br-none' + ' ' + 'rounded-br-none')  ;
     socket.emit('send' , message) ;
 
 })
 
 socket.on('left' , name =>{
-    append(`${name} left the chat` , 'self-start'   + ' ' + 'rounded-bl-none' + ' ' + 'md:rounded-bl-none' + 'rounded-br-xl' + ' ' + 'bg-red-300' + ' ' + 'text-black') ; 
+    append(`${name} left the chat` , 'self-start'   + ' ' + 'rounded-bl-none' + ' ' + 'md:rounded-bl-none' +  'rounded-br-xl' + ' ' + 'bg-red-300' + ' ' + 'text-black') ; 
 })
 
